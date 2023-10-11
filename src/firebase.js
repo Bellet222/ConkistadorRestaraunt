d69ebda6f91dcc0ff9d31c26cedcc793a8a06a0c
Daniil Startcev <104425223+Bellet222@users.noreply.github.com>
2023-10-11 10:39:50 +0500

    Update firebase.js
    confidencal info deleted

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDXARIgXI91vPNLNPFVeRguz2KyZSFkWHg",
+    apiKey: "api-key",
     authDomain: "restaraunt-julia.firebaseapp.com",
     projectId: "restaraunt-julia",
     storageBucket: "restaraunt-julia.appspot.com",
-    messagingSenderId: "1018531441157",
-    appId: "1:1018531441157:web:9ad282fdc53ca5911fca6c"
+    messagingSenderId: "id",
+    appId: "api=id"
 };
 
 const app = initializeApp(firebaseConfig);
